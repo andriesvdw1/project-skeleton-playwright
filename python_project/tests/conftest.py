df86231ed6857682cc8f8608fb531582c2dedd3e
Andries <andriesvdw1@gmail.com>
2026-06-29 19:39:37 +0200

    Update return statement of setup_load_page function on line 27 conftest.py

--- python_project/tests/conftest.py
+++ python_project/tests/conftest.py
@@ -17,8 +17,11 @@ def setup_playwright(playwright, request):
         log_message(logging.getLogger(), "closing browser", LogLevel.INFO)
         browser.close()
 
+
 @pytest.fixture()
 def setup_load_page(setup_playwright):
-        login_page =  LoginPage(setup_playwright)
-        login_page.navigate_to(URL)
-        log_message(logging.getLogger(), f"navigate to {URL}", LogLevel.INFO)
+    login_page = LoginPage(setup_playwright)
+    login_page.navigate_to(URL)
+    log_message(logging.getLogger(), f"navigate to {URL}", LogLevel.INFO)
+
+    return login_page  # <-- Add this line to pass the object to your test!
