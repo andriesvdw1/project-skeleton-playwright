2ac504942c06706ad5ca22718f8a0c8a977e3f9d
Andries <andriesvdw1@gmail.com>
2026-06-29 20:30:09 +0200

    Update test_login.py to use username and password from the VALID_CREDENTIALS config file.

--- python_project/tests/test_login.py
+++ python_project/tests/test_login.py
@@ -5,22 +5,4 @@ from python_project.helper.config import VALID_CREDENTIALS
 
 def test_successfully_login(setup_load_page):
     login_page = setup_load_page
-    login_page.perform_login(VALID_CREDENTIALS["email"], VALID_CREDENTIALS["password"])
-
-
-# open browser and load the login page
-# username entered
-# password entered
-# click login button
-# verify logged in successfully
-
-#test 2
-#[wrong username, valid password]
-#[valid username, wrong password]
-#[missing username, valid password]
-#[valid username, missing password]
-
-# open browser and load the login page
-# enter test case
-# press login
-# verify failed login
+    login_page.perform_login(VALID_CREDENTIALS["username"], VALID_CREDENTIALS["password"])
