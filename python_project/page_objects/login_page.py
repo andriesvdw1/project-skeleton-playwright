69b8ea12b54dbf6c1c34e55c207431e2de8e089f
Andries <andriesvdw1@gmail.com>
2026-06-29 19:49:01 +0200

    Update login_page.py to have correct imports and update line 18 and line 23 to use correct LogLevel values.

--- python_project/page_objects/login_page.py
+++ python_project/page_objects/login_page.py
@@ -4,7 +4,8 @@ from pytest_playwright.pytest_playwright import page
 from python_project.helper.utils import take_screenshot, log_message
 from python_project.page_objects.base_page import BasePage
 from python_project.page_objects.main_page import MainPage
-
+from python_project.helper.utils import LogLevel
+import logging
 
 class LoginPage(BasePage):
     def __init__(self, page : Page):
@@ -14,12 +15,12 @@ class LoginPage(BasePage):
         self.login_button = self.page.locator( "[name = 'login']")
 
     def perform_login(self, username: str, password: str):
-        log_message(self.logger, "performing login", level=logging.INFO)
+        log_message(self.logger, "performing login", level=LogLevel.INFO)
         self.type_text(self.username_field, username)
         self.type_text(self.password_field, password)
         self.click_element(self.login_button)
         if self.login_button.is_visible():
-            log_message(self.logger, "login failed", level=logging.ERROR)
+            log_message(self.logger, "login failed", level=LogLevel.ERROR)
             take_screenshot("login failed")
             return None
         return MainPage(self.page)
